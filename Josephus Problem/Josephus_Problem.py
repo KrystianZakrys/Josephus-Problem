1f668fcfc0065bc57129fbaf8451c035e9975b8d
krystian.zakrys <krystian.zakrys@gmail.com>
2018-12-26 00:01:59 +0100

    Binary approach for Josephus Problem. For now working for values below 48 [ binary mask issue, need to find better solution ]

--- Josephus Problem/Josephus_Problem.py
+++ Josephus Problem/Josephus_Problem.py
@@ -1,1 +1,22 @@
 
+import sys
+
+n =  48#sys.argv[1]
+#if n.isdecimal():
+print(bin(n))
+offset = n.bit_length()
+_n1 = n >> offset-1
+_n2 = n << 1
+print(bin(_n2))
+
+mask = 0b011111
+
+_n2 = _n2 & mask
+
+result = _n1 | _n2
+
+print(bin(n))
+print(bin(_n1))
+print(bin(_n2))
+print(bin(mask))
+print(result)
